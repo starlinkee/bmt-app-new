f903264b2c9f1119268b57593c450eb044f20606
viktor <vikbobinski@gmail.com>
2026-09-05 18:24:06 +0200

    x

--- download-fonts.js
+++ download-fonts.js
@@ -1,31 +1,21 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const fs = require('fs');
-const https = require('https');
 const path = require('path');
 
 const fontsDir = path.join(__dirname, 'public', 'fonts');
 if (!fs.existsSync(fontsDir)) fs.mkdirSync(fontsDir, { recursive: true });
 
-function downloadFont(url, filename) {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(path.join(fontsDir, filename));
-    https.get(url, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
-        resolve();
-      });
-    }).on('error', (err) => {
-      fs.unlink(path.join(fontsDir, filename), () => {});
-      reject(err);
-    });
-  });
+async function downloadFont(url, filename) {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(`Failed to fetch ${url}: ${res.statusText}`);
+  const buffer = await res.arrayBuffer();
+  fs.writeFileSync(path.join(fontsDir, filename), Buffer.from(buffer));
 }
 
 async function main() {
   console.log('Downloading fonts...');
-  await downloadFont('https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto-Regular.ttf', 'Roboto-Regular.ttf');
-  await downloadFont('https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto-Bold.ttf', 'Roboto-Bold.ttf');
+  await downloadFont('https://raw.githubusercontent.com/googlefonts/roboto/main/src/hinted/Roboto-Regular.ttf', 'Roboto-Regular.ttf');
+  await downloadFont('https://raw.githubusercontent.com/googlefonts/roboto/main/src/hinted/Roboto-Bold.ttf', 'Roboto-Bold.ttf');
   console.log('Fonts downloaded.');
 }
 main();
